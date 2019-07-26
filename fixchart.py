1b098786c839360ec99333a99887d8c31ad1e842
Jordan Fox <fox.1061@osu.edu>
2019-07-25 21:16:06 -0400

    changed logic for add_address_2

--- fixchart.py
+++ fixchart.py
@@ -2,71 +2,53 @@ import sys
 
 #------------------------------ Address 2 stuff ------------------------------------#
 
-# helper method to fix one line
-def fixline(line, string, startline, line_no):
-	strindex = line.index(string)
-	afterstr = line[line.index(string):len(line)]
-	firstcommaindex = afterstr.index(",")
-	#print("Fixed line " + str(line_no))
-	return startline + line[0:strindex  - 1] + ","  + afterstr[0:firstcommaindex] + afterstr[firstcommaindex + 1:len(afterstr)]
-
 # separate Address 2 part from Address column
 # Takes Apt, Unit, Ste, #, Fl, Ph out of Address column and puts it into the next column
 # Note: for this to work properly there must be an empty column to the right of Address
-# TODO: only fix lines where address_2 string is found in Address column (currently
-# 		searches the entire line)
 # TODO: only fix lines where address_2 string appears at the end of the address column
 # 		(fixes bug when adress_2 string appears in street name, etc.)
-def add_address_2(filename):
-	return 0
-
-
-	''' TODO: rework to use rows[][]
-	print("Fixing lines...")
+def add_address_2(rows):
 	line_no = 0
+	for row in rows:
+		address = row[1]
 
-	readfile = open(filename)
-	lines = readfile.read().splitlines()
-	writefile = open("fixed.csv", 'w')
-	
-	for someline in lines:
-		startline = someline[0:someline.index(",")]
-		line = someline[someline.index(","):len(someline)]
-		
-		if("Apt" in line):
-			someline = fixline(line, "Apt", startline, line_no)
-		if("Unit" in line):
-			someline = fixline(line, "Unit", startline, line_no)
-		if("Ste" in line):
-			someline = fixline(line, "Ste", startline, line_no)
-		if("#" in line):
-			someline = fixline(line, "#", startline, line_no)
-		if("Fl" in line):
-			someline = fixline(line, "Fl", startline, line_no)
-		if("Ph" in line):
-			someline = fixline(line, "Ph", startline, line_no)
-			
-		writefile.write(someline + "\n")
-	
-		line_no = line_no + 1
-		#print("Checked line " + str(line_no))	
+		if("Apt" in address):
+			index = address.index("Apt")
+			row[1] = address[0:index]
+			row[2] = address[index:]
+		if("Unit" in address):
+			index = address.index("Unit")
+			row[1] = address[0:index]
+			row[2] = address[index:]
+		if("Ste" in address):
+			index = address.index("Ste")
+			row[1] = address[0:index]
+			row[2] = address[index:]
+		if("#" in address):
+			index = address.index("#")
+			row[1] = address[0:index]
+			row[2] = address[index:]
+		if("Fl" in address):
+			index = address.index("Fl")
+			row[1] = address[0:index]
+			row[2] = address[index:]
+		if("Ph" in address):
+			index = address.index("Ph")
+			row[1] = address[0:index]
+			row[2] = address[index:]
 
-	readfile.close()
-	writefile.close()
-	'''
-	
+		line_no += 1
+
 #-------------------------- duplicate lname + address stuff ------------------------#
 	
 def get_lname_address_tuples(lines):
 	return 0
 	
 
-def remove_extra_lname_address(filename):
+def remove_extra_lname_address(rows):
 	return 0
 
-
 #-------------------------- helper functions ---------------------------------------#
-
 
 # takes an array of csv lines and parses it to a multidimensional array
 def parse_csv_lines(lines):
@@ -77,6 +59,17 @@ def parse_csv_lines(lines):
 		
 	return rows
 
+# write a multidimensional array to a csv
+def write_to_csv(rows):
+	writefile = open("fixed.csv", 'w')
+
+	for row in rows:
+		string = ""
+		for i in row:
+			string += i + ","
+		writefile.write(string[0:len(string) - 1] + "\n")
+
+	writefile.close()
 	
 # ------------------------------ execute script ------------------------------------#
 
@@ -99,10 +92,13 @@ print(rows[0:10])
 
 if "-a" in sys.argv:
 	print("Operation: Add Address 2 column")
-	add_address_2(filename)
+	add_address_2(rows)
 
 if "-r" in sys.argv:
 	print("Operation: Remove rows with same Last Name + Address")
-	remove_extra_lname_address(filename)
+	remove_extra_lname_address(rows)
+
+write_to_csv(rows)
 	
+readfile.close()
 print("Done.")
