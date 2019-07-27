cb83bcfdfae69dcbcd04b23e76d809555b32435b
jrdanfox <fox.1061@osu.edu>
2019-07-27 14:22:31 -0400

    add logic to remove extra lname, address pairs

--- fixchart.py
+++ fixchart.py
@@ -1,52 +1,50 @@
-import sys
+import sys, collections
 
 #------------------------------ Address 2 stuff ------------------------------------#
 
 # separate Address 2 part from Address column
 # Takes Apt, Unit, Ste, #, Fl, Ph out of Address column and puts it into the next column
 # Note: for this to work properly there must be an empty column to the right of Address
-# TODO: only fix lines where address_2 string appears at the end of the address column
-# 		(fixes bug when adress_2 string appears in street name, etc.)
 def add_address_2(rows):
 	line_no = 0
+	address_2_strings = ["Apt ", "Unit ", "Ste ", "#", "Fl ", "Ph "]
 	for row in rows:
 		address = row[1]
 
-		if("Apt " in address):
-			index = address.index("Apt ")
-			row[1] = address[0:index]
-			row[2] = address[index:]
-		if("Unit " in address):
-			index = address.index("Unit ")
-			row[1] = address[0:index]
-			row[2] = address[index:]
-		if("Ste " in address):
-			index = address.index("Ste ")
-			row[1] = address[0:index]
-			row[2] = address[index:]
-		if("#" in address):
-			index = address.index("#")
-			row[1] = address[0:index]
-			row[2] = address[index:]
-		if("Fl " in address):
-			index = address.index("Fl ")
-			row[1] = address[0:index]
-			row[2] = address[index:]
-		if("Ph " in address):
-			index = address.index("Ph ")
-			row[1] = address[0:index]
-			row[2] = address[index:]
+		for address_2_string in address_2_strings:
+			if (address_2_string in address):
+				index = address.index(address_2_string)
+				row[1] = address[0:index]
+				row[2] = address[index:]
 
 		line_no += 1
 
 #-------------------------- duplicate lname + address stuff ------------------------#
 	
-def get_lname_address_tuples(lines):
-	return 0
-	
 
+# returns a dictionary of {(address, lname), (count(address, lname), line_no)}
+# TODO: maybe use (row[1].strip(), row[8].strip()) if whitespaces causes issues
+#			hint: "a " != "a"
+# TODO: dynamically get address, lname columns
+def get_lname_address_pairs(rows):
+	pairs = collections.OrderedDict()
+	for i, row in enumerate(rows, start = 0):
+		pair = (row[1], row[8])
+		default_value = (1, i)
+		if pair in pairs:
+			pairs[pair] = (pairs[pair][0] + 1, i)
+		else:
+			pairs[pair] = default_value
+	return pairs
+	
 def remove_extra_lname_address(rows):
-	return 0
+	pairs = get_lname_address_pairs(rows)
+
+	for x, y in reversed(pairs.items()):
+		if y[0] > 1 and x[0] != "" and x[1] != "":
+			rows[y[1] - 1][5] = rows[y[1]][4]
+			rows[y[1] - 1][7] = rows[y[1]][6]
+			del rows[y[1]]
 
 #-------------------------- helper functions ---------------------------------------#
 
@@ -88,7 +86,6 @@ print("Fixing file: " + filename)
 readfile = open(filename)
 lines = readfile.read().splitlines()
 rows = parse_csv_lines(lines)
-print(rows[0:10])
 
 if "-a" in sys.argv:
 	print("Operation: Add Address 2 column")
